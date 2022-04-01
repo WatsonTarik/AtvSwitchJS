d96c08c425ed2ee27c541f9e1c2cf8f48f97ac05
WatsonTarik <99353486+WatsonTarik@users.noreply.github.com>
2022-03-31 23:29:16 -0300

    Add files via upload

---
+++ AtvPW.js
@@ -0,0 +1,222 @@
+function a() {
+    let a = 1;        
+
+    switch (a){
+        case 1: {
+    document.querySelector("#ds1").style.display = "block";
+    document.querySelector("#ds2").style.display = "none";
+    document.querySelector("#ds3").style.display = "none";
+    document.querySelector("#ds4").style.display = "none";
+    document.querySelector("#ds5").style.display = "none";
+    document.querySelector("#ds6").style.display = "none";
+    document.querySelector("#ds7").style.display = "none";
+    document.querySelector("#ds8").style.display = "none";
+    document.querySelector("#ds9").style.display = "none";
+    document.querySelector("#ds10").style.display = "none";
+    document.querySelector("#ds11").style.display = "none";
+        }
+    }
+}
+
+function b() {
+    let b = 1;
+
+    switch (b){
+        case 1: {
+            document.querySelector("#ds1").style.display = "none";
+            document.querySelector("#ds2").style.display = "block";
+            document.querySelector("#ds3").style.display = "none";
+            document.querySelector("#ds4").style.display = "none";
+            document.querySelector("#ds5").style.display = "none";
+            document.querySelector("#ds6").style.display = "none";
+            document.querySelector("#ds7").style.display = "none";
+            document.querySelector("#ds8").style.display = "none";
+            document.querySelector("#ds9").style.display = "none";
+            document.querySelector("#ds10").style.display = "none";
+            document.querySelector("#ds11").style.display = "none";
+        }
+    }
+}
+
+function c() {
+    let c = 1;
+
+    switch (c){
+        case 1: {
+            document.querySelector("#ds1").style.display = "none";
+            document.querySelector("#ds2").style.display = "none";
+            document.querySelector("#ds3").style.display = "block";
+            document.querySelector("#ds4").style.display = "none";
+            document.querySelector("#ds5").style.display = "none";
+            document.querySelector("#ds6").style.display = "none";
+            document.querySelector("#ds7").style.display = "none";
+            document.querySelector("#ds8").style.display = "none";
+            document.querySelector("#ds9").style.display = "none";
+            document.querySelector("#ds10").style.display = "none";
+            document.querySelector("#ds11").style.display = "none";
+        }
+    }
+}
+
+function d() {
+    let d = 1;
+
+    switch (d){
+        case 1: {
+            document.querySelector("#ds1").style.display = "none";
+            document.querySelector("#ds2").style.display = "none";
+            document.querySelector("#ds3").style.display = "none";
+            document.querySelector("#ds4").style.display = "block";
+            document.querySelector("#ds5").style.display = "none";
+            document.querySelector("#ds6").style.display = "none";
+            document.querySelector("#ds7").style.display = "none";
+            document.querySelector("#ds8").style.display = "none";
+            document.querySelector("#ds9").style.display = "none";
+            document.querySelector("#ds10").style.display = "none";
+            document.querySelector("#ds11").style.display = "none";
+        }
+    }
+}
+
+function e() {
+    let e = 1;
+
+    switch (e){
+        case 1: {
+            document.querySelector("#ds1").style.display = "none";
+            document.querySelector("#ds2").style.display = "none";
+            document.querySelector("#ds3").style.display = "none";
+            document.querySelector("#ds4").style.display = "none";
+            document.querySelector("#ds5").style.display = "block";
+            document.querySelector("#ds6").style.display = "none";
+            document.querySelector("#ds7").style.display = "none";
+            document.querySelector("#ds8").style.display = "none";
+            document.querySelector("#ds9").style.display = "none";
+            document.querySelector("#ds10").style.display = "none";
+            document.querySelector("#ds11").style.display = "none";
+        }
+    }
+}
+
+function f() {
+    let f = 1;
+
+    switch (f){
+        case 1: {
+            document.querySelector("#ds1").style.display = "none";
+            document.querySelector("#ds2").style.display = "none";
+            document.querySelector("#ds3").style.display = "none";
+            document.querySelector("#ds4").style.display = "none";
+            document.querySelector("#ds5").style.display = "none";
+            document.querySelector("#ds6").style.display = "block";
+            document.querySelector("#ds7").style.display = "none";
+            document.querySelector("#ds8").style.display = "none";
+            document.querySelector("#ds9").style.display = "none";
+            document.querySelector("#ds10").style.display = "none";
+            document.querySelector("#ds11").style.display = "none";
+        }
+    }
+}
+
+function g() {
+    let g = 1;
+
+    switch (g){
+        case 1: {
+            document.querySelector("#ds1").style.display = "none";
+            document.querySelector("#ds2").style.display = "none";
+            document.querySelector("#ds3").style.display = "none";
+            document.querySelector("#ds4").style.display = "none";
+            document.querySelector("#ds5").style.display = "none";
+            document.querySelector("#ds6").style.display = "none";
+            document.querySelector("#ds7").style.display = "block";
+            document.querySelector("#ds8").style.display = "none";
+            document.querySelector("#ds9").style.display = "none";
+            document.querySelector("#ds10").style.display = "none";
+            document.querySelector("#ds11").style.display = "none";
+        }
+    }
+}
+
+function h() {
+    let h = 1;
+
+    switch (h){
+        case 1: {
+            document.querySelector("#ds1").style.display = "none";
+            document.querySelector("#ds2").style.display = "none";
+            document.querySelector("#ds3").style.display = "none";
+            document.querySelector("#ds4").style.display = "none";
+            document.querySelector("#ds5").style.display = "none";
+            document.querySelector("#ds6").style.display = "none";
+            document.querySelector("#ds7").style.display = "none";
+            document.querySelector("#ds8").style.display = "block";
+            document.querySelector("#ds9").style.display = "none";
+            document.querySelector("#ds10").style.display = "none";
+            document.querySelector("#ds11").style.display = "none";
+        }
+    }
+}
+
+function i() {
+    let i = 1;
+
+    switch (i){
+        case 1: {
+            document.querySelector("#ds1").style.display = "none";
+            document.querySelector("#ds2").style.display = "none";
+            document.querySelector("#ds3").style.display = "none";
+            document.querySelector("#ds4").style.display = "none";
+            document.querySelector("#ds5").style.display = "none";
+            document.querySelector("#ds6").style.display = "none";
+            document.querySelector("#ds7").style.display = "none";
+            document.querySelector("#ds8").style.display = "none";
+            document.querySelector("#ds9").style.display = "block";
+            document.querySelector("#ds10").style.display = "none";
+            document.querySelector("#ds11").style.display = "none";
+        }
+    }
+}
+
+function j() {
+    let j = 1;
+
+    switch (j){
+        case 1: {
+            document.querySelector("#ds1").style.display = "none";
+            document.querySelector("#ds2").style.display = "none";
+            document.querySelector("#ds3").style.display = "none";
+            document.querySelector("#ds4").style.display = "none";
+            document.querySelector("#ds5").style.display = "none";
+            document.querySelector("#ds6").style.display = "none";
+            document.querySelector("#ds7").style.display = "none";
+            document.querySelector("#ds8").style.display = "none";
+            document.querySelector("#ds9").style.display = "none";
+            document.querySelector("#ds10").style.display = "block";
+            document.querySelector("#ds11").style.display = "none";
+        }
+    }
+}
+
+function k() {
+    let k = 1;
+
+    switch (k){
+        case 1: {
+            document.querySelector("#ds1").style.display = "none";
+            document.querySelector("#ds2").style.display = "none";
+            document.querySelector("#ds3").style.display = "none";
+            document.querySelector("#ds4").style.display = "none";
+            document.querySelector("#ds5").style.display = "none";
+            document.querySelector("#ds6").style.display = "none";
+            document.querySelector("#ds7").style.display = "none";
+            document.querySelector("#ds8").style.display = "none";
+            document.querySelector("#ds9").style.display = "none";
+            document.querySelector("#ds10").style.display = "none";
+            document.querySelector("#ds11").style.display = "block";
+        }
+    }
+}
+
+// break;
+// default:
